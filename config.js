227d370606be87d386b9bc4d84d11f6f08b05187
tohatsuwahyu <tohatsu.wbp@gmail.com>
2025-10-29 09:38:45 +0900

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,6 @@
 /* config.js — simpan sejajar dengan index.html */
 window.CONFIG = {
   BASE_URL: 'https://script.google.com/macros/s/AKfycbxHqbEC6XasKe_Ieei-_e5UqCi_Vrskg-l_WgIdLVzrthkqoKKuv0Jn6bUHzl0UUDJo/exec',
-  API_KEY : 'supersecret123'
+  API_KEY : 'supersecret123',
+  LOGO_URL: 'assets/tsh.png'
 };
