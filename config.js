950f8d014b2e4c7c7dec08438ff82e1513c17399
tohatsuwahyu <tohatsu.wbp@gmail.com>
2025-11-11 10:11:12 +0900

    Update config.js

--- config.js
+++ config.js
@@ -3,4 +3,16 @@ window.CONFIG = {
   BASE_URL: 'https://script.google.com/macros/s/AKfycbxHqbEC6XasKe_Ieei-_e5UqCi_Vrskg-l_WgIdLVzrthkqoKKuv0Jn6bUHzl0UUDJo/exec',
   API_KEY : 'supersecret123',
   LOGO_URL: 'assets/tsh.png'
+,
+  FEATURES: {
+    LOW_STOCK_ALERT: true,
+    BULK_ACTIONS: true,
+    INLINE_EDIT: true,
+    QUICK_SCAN: true,
+    HEALTH_PING: true,
+    SKELETON: true,
+    FEATURE_TOUR: true
+  },
+  IDLE_MIN: 20,
+  HEALTH_PING_MS: 15000
 };
